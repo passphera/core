328fdf9081c4f268b5bd90592c3cee9b6d8b1d44
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2024-04-03 04:52:52 +0100

    begine the real work, the first touches

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='passphera-core',
+    version='0.0.1',
+    author='Fathi Abdelmalek',
+    author_email='abdelmalek.fathi.2001@gmail.com',
+    url='',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=[''],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 Only",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+    ]
+)

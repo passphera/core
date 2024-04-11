75bb353e7285f8664f72bc7329cbf8b47444e6d1
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2024-04-11 01:07:37 +0100

    update files structure and remove a bug in generate_password function

--- passphera_core/__init__.py
+++ passphera_core/__init__.py
@@ -1,2 +1,2 @@
-from generator import PasswordGenerator as PasswordGenerator
-from exceptions import InvalidAlgorithmException as InvalidAlgorithmException
+from .generator import PasswordGenerator as PasswordGenerator
+from .exceptions import InvalidAlgorithmException as InvalidAlgorithmException

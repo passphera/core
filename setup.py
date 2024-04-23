fb1bfdfb2e447ffa46ed788a2775b501af7d0943
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2024-04-23 23:53:09 +0100

    just a tiny modification in the generation process, now the characters replacements will done after the upper case changing process instead of before

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='passphera-core',
-    version='0.3.3',
+    version='0.3.4',
     author='Fathi Abdelmalek',
     author_email='abdelmalek.fathi.2001@gmail.com',
     url='https://github.com/passphera/core',

031fed79b3d10992fec4266a3b2f55acb3fbefec
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2025-01-07 17:49:04 +0100

    refactor the code, now only implementation of use cases is left

---
+++ passphera_core/interfaces.py
@@ -0,0 +1,84 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from passphera_core.entities import Password, Generator, GeneratorConfig, User
+
+
+class PasswordRepository(ABC):
+    @abstractmethod
+    def save(self, password: Password) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, password: Password) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, password_id: UUID) -> None:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, password_id: UUID) -> Password:
+        pass
+
+    @abstractmethod
+    def find_by_context(self, password_context: str) -> Password:
+        pass
+
+
+class GeneratorRepository(ABC):
+    @abstractmethod
+    def save(self, generator: Generator) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, generator: Generator) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, generator_id: UUID) -> None:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, generator_id: UUID) -> Generator:
+        pass
+
+
+class GeneratorConfigRepository(ABC):
+    @abstractmethod
+    def save(self, generator_config: GeneratorConfig) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, generator_config: GeneratorConfig) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, generator_config_id: UUID) -> None:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, generator_config_id: UUID) -> GeneratorConfig:
+        pass
+
+
+class UserRepository(ABC):
+    @abstractmethod
+    def save(self, user: User) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, user: User) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, user_id: UUID) -> None:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, user_id: UUID) -> User:
+        pass
+
+    @abstractmethod
+    def find_by_username(self, username: str) -> User:
+        pass

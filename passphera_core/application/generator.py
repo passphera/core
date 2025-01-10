79217fb5a7da691808367d53048bd09f195f35ff
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2025-01-10 22:48:11 +0100

    add GetGeneratorUseCase and GetGeneratorConfigPropertyUseCase, and complete the project

--- passphera_core/application/generator.py
+++ passphera_core/application/generator.py
@@ -5,6 +5,25 @@ from passphera_core.entities import Generator, GeneratorConfig
 from passphera_core.interfaces import GeneratorRepository, GeneratorConfigRepository
 
 
+class GetGeneratorUseCase:
+    def __init__(self, generator_repository: GeneratorRepository):
+        self.generator_repository: GeneratorRepository = generator_repository
+
+    def execute(self, user_id: UUID) -> Generator:
+        return self.generator_repository.find_by_user_id(user_id)
+    
+    
+class GetGeneratorConfigPropertyUseCase:
+    def __init__(self, generator_repository: GeneratorRepository, generator_config_repository: GeneratorConfigRepository):
+        self.generator_repository: GeneratorRepository = generator_repository
+        self.generator_config_repository: GeneratorConfigRepository = generator_config_repository
+
+    def execute(self, user_id: UUID, field: str) -> str:
+        generator_entity: Generator = self.generator_repository.find_by_user_id(user_id)
+        generator_config_entity: GeneratorConfig = self.generator_config_repository.find_by_generator_id(generator_entity.id)
+        return getattr(generator_config_entity, field)
+
+
 class UpdateGeneratorConfigUseCase:
     def __init__(
             self,
